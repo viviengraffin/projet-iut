d53c0772aca6be6dce52a43f81de6f654e2be22e
Vivien Graffin <vivigraffin@gmail.com>
2017-10-16 16:41:13 +0200

    suppression de choses inutiles 2

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 	session_start();
 	include("include/all.php");
 	
-	$mvc->state("form1","form1");
-	$mvc->state("resform1","resform1","resform1");
-	$mvc->state("testdf","testdf");
+	$mvc->state("inscription","inscription");
 	
 	$mvc->dstate("error404","error404");
 	
