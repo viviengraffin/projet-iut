e5eb2464af0179af3ff3d076d106981ca1582131
Vivien Graffin <vivigraffin@gmail.com>
2017-11-28 23:41:54 +0100

    Chronomètre

--- js/chronometre.js
+++ js/chronometre.js
@@ -1,51 +1,99 @@
 var chronometre={}
 chronometre.chronos=[]
 
+
 chronometre.createChrono=function(){
 	var chrono={}
 	chrono.style="Il reste $m minutes et $s secondes"
+	chrono.asc=false
 	return(chrono)
 }
-
 chronometre.init=function(){
-	htmls=document.querySelectorAll("span[chrono-second]")
+	htmls=document.querySelectorAll("span[chrono-time]")
 	htmls.forEach(function(html){
 		var chrono={}
 		chrono=chronometre.createChrono()
 		chrono.html=html
-		chrono.minut=html.getAttribute("chrono-minut") || 0
-		chrono.second=html.getAttribute("chrono-second")
+		var t=chronometre.getTime(html.getAttribute("chrono-time"))
+		chrono.minut=t[0]
+		chrono.second=t[1]
+		chrono.defaultMinut=t[0]
+		chrono.defaultSecond=t[1]
+		chrono.tcdefMinut=chronometre.get0time(chrono.minut)
+		chrono.tcdefSecond=chronometre.get0time(chrono.second)
+		if(html.hasAttribute("chrono-asc")){
+			chrono.asc=true
+			chrono.minut=0
+			chrono.second=0
+		}
 		if(html.innerText!=""){
 			chrono.style=html.innerText
 		}
 		chrono.setText=function(){
-			var s=chrono.minut+"m"+chrono.second+"s"
+			var minut=chrono.minut
+			var second=chrono.second
+			var s=minut+"m"+second+"s"
+			var rts=chronometre.getRTString(chrono)
 			if(chrono.html.hasAttribute("chrono-color-"+s)){
 				chrono.html.style.color=chrono.html.getAttribute("chrono-color-"+s)
 			}
 			if(chrono.html.hasAttribute("chrono-style-"+s)){
 				chrono.style=chrono.html.getAttribute("chrono-style-"+s)
 			}
-			var res=chrono.style.replace("$m",chrono.minut)
-			res=res.replace("$s",chrono.second)
-			res=res.replace("$M",chronometre.get0time(chrono.minut))
-			res=res.replace("$S",chronometre.get0time(chrono.second))
+			if(chrono.html.hasAttribute("chrono-style-"+rts)){
+				chrono.style=chrono.html.getAttribute("chrono-style-"+rts)
+			}
+			if(chrono.html.hasAttribute("chrono-color-"+rts)){
+				chrono.html.style.color=chrono.html.getAttribute("chrono-color-"+rts)
+			}
+			var p=chronometre.getPourcent(chrono)
+			var res=chrono.style.replace("$m",minut)
+			res=res.replace("$s",second)
+			res=res.replace("$M",chronometre.get0time(minut))
+			res=res.replace("$S",chronometre.get0time(second))
+			res=res.replace("$mdef",chrono.defaultMinut)
+			res=res.replace("$sdef",chrono.defaultSecond)
+			res=res.replace("$Sdef",chrono.tcdefSecond)
+			res=res.replace("$Mdef",chrono.tcdefMinut)
+			res=res.replace("$p",p)
+			res=res.replace("$P",chronometre.get0time(p))
 			chrono.html.innerText=res
 		}
 		chrono=chronometre.initChrono(chrono)
 		chrono.setText()
 		chrono.interval=setInterval(function(){
 			var good=true
-			if(chrono.second>0){
-				chrono.second--
-			}
-			else if(chrono.minut>0){
-				chrono.minut--
-				chrono.second=59
+			if(chrono.asc){
+				if(chrono.second<59){
+					if((chrono.minut==chrono.defaultMinut)&&(chrono.second>chrono.defaultSecond-1)){
+						clearInterval(chrono.interval)
+						good=false
+					}
+					else{
+						chrono.second++
+					}
+				}
+				else if(chrono.minut<chrono.defaultMinut){
+					chrono.second=0
+					chrono.minut++
+				}
+				else{
+					clearInterval(chrono.interval)
+					good=false
+				}
 			}
 			else{
-				clearInterval(chrono.interval)
-				good=false
+				if(chrono.second>0){
+					chrono.second--
+				}
+				else if(chrono.minut>0){
+					chrono.minut--
+					chrono.second=59
+				}
+				else{
+					clearInterval(chrono.interval)
+					good=false
+				}
 			}
 			if(good){
 				chrono.setText()
@@ -68,15 +116,29 @@ chronometre.initChrono=function(chrono){
 	while(i<attributes.length){
 		attribute=attributes[i]
 		if(attribute.name.substr(0,12)=="chrono-color"){
-			var t=chronometre.getTime(attribute.name.substr(13,attribute.name.length-13))
-			if((t[0]>=chrono.minut)&&(t[1]>=chrono.second)){
-				chrono.html.style.color=attribute.value
+			var t=chronometre.getTimeT(attribute.name.substr(13,attribute.name.length-13),chrono)
+			if(!chrono.asc){
+				if((t[0]>=chrono.minut)&&(t[1]>=chrono.second)){
+					chrono.html.style.color=attribute.value
+				}
+			}
+			else{
+				if((t[0]<=chrono.minut)&&(t[1]<=chrono.second)){
+					chrono.html.style.color=attribute.value
+				}
 			}
 		}
 		else if(attribute.name.substr(0,12)=="chrono-style"){
-			var t=chronometre.getTime(attribute.name.substr(13,attribute.name.length-13))
-			if((t[0]>=chrono.minut)&&(t[1]>=chrono.second)){
-				chrono.style=attribute.value
+			var t=chronometre.getTimeT(attribute.name.substr(13,attribute.name.length-13),chrono)
+			if(!chrono.asc){
+				if((t[0]>=chrono.minut)&&(t[1]>=chrono.second)){
+					chrono.style=attribute.value
+				}
+			}
+			else{
+				if((t[0]<=chrono.minut)&&(t[2]<=chrono.second)){
+					chrono.style=attribute.value
+				}
 			}
 		}
 		i++
@@ -104,7 +166,75 @@ chronometre.getTime=function(time){
 		}
 		i++
 	}
-	return([minut,second])
+	return([parseInt(minut),parseInt(second)])
+}
+chronometre.getTimeT=function(time,chrono){
+	if(time[0]=="r"){
+		if(chrono.asc){
+			var r=chronometre.getTime(time.substr(1,time.length-1))
+			var def=60*chrono.defaultMinut+chrono.defaultSecond
+			var actual=60*r[0]+r[1]
+			var diff=def-actual
+			
+			var minut=0
+			var good=false
+			while(!good){
+				if(diff>=60){
+					minut++
+					diff-=60
+				}
+				else{
+					good=true
+				}
+			}
+			return([minut,diff])
+		}
+		else{
+			return(chronometre.getTime(time.substr(1,time.length-1)))
+		}
+	}
+	else{
+		return(chronometre.getTime(time))
+	}
+}
+chronometre.getRTString=function(chrono){
+	if(chrono.asc){
+		var actual=60*chrono.minut+chrono.second
+		var def=60*chrono.defaultMinut+chrono.defaultSecond
+		var diff=def-actual
+		var minut=0
+		var good=false
+		
+		while(!good){
+			if(diff>=60){
+				minut++
+				diff-=60
+			}
+			else{
+				good=true
+			}
+		}
+	}
+	else{
+		var minut=chrono.minut
+		var diff=chrono.second
+	}
+	return("r"+minut+"m"+diff+"s")
+}
+chronometre.getPourcent=function(chrono){
+	if(chrono.asc){
+		var def=60*chrono.defaultMinut+chrono.defaultSecond
+		var actual=60*chrono.minut+chrono.second
+		var div=actual/def
+		return(parseInt(div*100))
+	}
+	else{
+		var def=60*chrono.defaultMinut+chrono.defaultSecond
+		var actual=60*chrono.minut+chrono.second
+		actual=def-actual
+		var div=actual/def
+		return(parseInt(div*100))
+	}
 }
 
 chronometre.init()
--- view/admin.php
+++ view/admin.php
@@ -5,8 +5,8 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
-		<span chrono-minut="0" chrono-second="11" chrono-color-0m59s="orange" chrono-color-0m10s="red" chrono-style-0m59s="$s secondes"chrono-style-0m0s="Fini">$m et $s secondes</span><br/>
-		<span chrono-minut="0" chrono-second="12" chrono-color-0m59s="purple" chrono-color-0m0s="red" chrono-style-0m0s="Terminé">$M:$S</span>
+		<span chrono-time="<?= $minut; ?>m<?= $second; ?>s" chrono-color-r0m15s="orange" chrono-style-r0m0s="Fin" chrono-color-r0m10s="red" chrono-style-1m0s="$m minutes et $s secondes" chrono-asc>$s secondes</span><br/>
+		<span chrono-time="1m5s" chrono-color-0m59s="orange" chrono-style-r0m0s="Fin" chrono-color-r0m10s="red" chrono-style-r0m59s="$s secondes">$m minutes et $s secondes/$mdef minutes et $sdef secondes</span><br/>
 		<?php load_view("header"); ?>
 		<?= $form->print(); ?>
 		<?php load_view("footer"); ?>
