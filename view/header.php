d506083d00c11713f58693099e93924d2b8e9200
Vivien Graffin <vivigraffin@gmail.com>
2017-12-04 15:56:13 +0100

    Header

--- view/header.php
+++ view/header.php
@@ -1,3 +1,13 @@
 <div class="header">
-	<h1>Projet iut</h1>
+	<div class="header-title">
+		Projet iut
+	</div>
+	<div class="header-menu">
+		<ul>
+			<li class="header-menu"><a href="inscription" class="header-menu">Inscription</a></li>
+			<li class="header-menu"><a href="ajoutRapporteur" class="header-menu">Ajout de rapporteur</a></li>
+			<li class="header-menu"><a href="addDossier" class="header-menu">Ajout de dossier</a></li>
+			<li class="header-menu"><a href="disconnect" class="header-menu">Déconnexion</a></li>
+		</ul>
+	</div>
 </div>
