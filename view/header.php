5e3bea65e6ad25dd246911f02b19099ff60c5767
Vivien Graffin <vivigraffin@gmail.com>
2017-12-13 08:53:09 +0100

    Lien du header

--- view/header.php
+++ view/header.php
@@ -3,13 +3,13 @@
                   
                     <a href="Aide.php" class="bouton_rouge"> <img src="images/flecheblanchegauche.png" alt="" /> Guide utilisateur </a>
 			
-		    <a href="Inscription.php" class="bouton_rouge"> <img src="https://png.icons8.com/s'inscrire/androidL/16/ffffff"> Inscription </a>
-		    <a href="Connexion.php" class="bouton_rouge"> <img src="https://png.icons8.com/nom/ios7/16/ffffff"> Connexion </a>
-		    <a href="Dossier.php" class="bouton_rouge"> <img src="https://png.icons8.com/inscription-d'étudiant/ios7/16/ffffff"> Saisie candidat </a>	
-                   <a href="Vote.php" class="bouton_rouge"> <img src="https://png.icons8.com/élections/ios7/16/ffffff"> Vote </a>
-		   <a href="Résultat.php" class="bouton_rouge"> <img src="https://png.icons8.com/statistiques/ios7/16/ffffff"> Résultat </a>
+		    <a href="inscription" class="bouton_rouge"> <img src="https://png.icons8.com/s'inscrire/androidL/16/ffffff"> Inscription </a>
+		    <a href="./" class="bouton_rouge"> <img src="https://png.icons8.com/nom/ios7/16/ffffff"> Connexion </a>
+		    <a href="addDossier" class="bouton_rouge"> <img src="https://png.icons8.com/inscription-d'étudiant/ios7/16/ffffff"> Saisie candidat </a>	
+                   <a href="vote" class="bouton_rouge"> <img src="https://png.icons8.com/élections/ios7/16/ffffff"> Vote </a>
+		   <a href="resultat" class="bouton_rouge"> <img src="https://png.icons8.com/statistiques/ios7/16/ffffff"> Résultat </a>
 			
-		   <a href="Deconnexion.php" class="bouton_rouge"> Déconnexion <img src="images/flecheblanchedroite.png" alt="" /></a>
+		   <a href="disconnect" class="bouton_rouge"> Déconnexion <img src="images/flecheblanchedroite.png" alt="" /></a>
 </div>	
 
               
