2df3b03be789157d604c865b5721f8ba82a7f4a4
Girlpower <32454792+mameFatou1@users.noreply.github.com>
2017-12-13 08:38:51 +0100

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,14 +1,18 @@
-<div class="header">
-	<div class="header-title">
-		Projet iut
-	</div>
-	<div class="header-menu">
-		<ul>
-			<li><a href="inscription">Inscription</a></li>
-			<li><a href="addDossier">Ajout de dossier</a></li>
-			<li><a href="admin">Espace Administration</a></li>
-			<li><a href="account">Compte</a></li>
-			<li><a href="disconnect">Déconnexion</a></li>
-		</ul>
-	</div>
-</div>
+            <div id="banniere_image">
+                <div id="banniere_description">
+                  
+                    <a href="Aide.php" class="bouton_rouge"> <img src="images/flecheblanchegauche.png" alt="" /> Guide utilisateur </a>
+			
+		    <a href="Inscription.php" class="bouton_rouge"> <img src="https://png.icons8.com/s'inscrire/androidL/16/ffffff"> Inscription </a>
+		    <a href="Connexion.php" class="bouton_rouge"> <img src="https://png.icons8.com/nom/ios7/16/ffffff"> Connexion </a>
+		    <a href="Dossier.php" class="bouton_rouge"> <img src="https://png.icons8.com/inscription-d'étudiant/ios7/16/ffffff"> Saisie candidat </a>	
+                   <a href="Vote.php" class="bouton_rouge"> <img src="https://png.icons8.com/élections/ios7/16/ffffff"> Vote </a>
+		   <a href="Résultat.php" class="bouton_rouge"> <img src="https://png.icons8.com/statistiques/ios7/16/ffffff"> Résultat </a>
+			
+		   <a href="Deconnexion.php" class="bouton_rouge"> Déconnexion <img src="images/flecheblanchedroite.png" alt="" /></a>
+</div>	
+
+              
+            </div>
+            
+          
